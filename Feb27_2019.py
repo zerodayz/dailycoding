5b6f89cb2c2679c2f58b1d1e878e772ba4568979
Robin Cernin <cerninr@gmail.com>
2019-02-27 11:38:56 +1000

    Fixed shadowing and name of the function
    Signed-off-by: Robin Cernin <cerninr@gmail.com>

--- Feb27_2019.py
+++ Feb27_2019.py
@@ -4,25 +4,25 @@
 # Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-mylist = [11, 15, 3, 7]
+mylist = [10, 15, 3, 7]
 k = 17
 
 # Brute force method where we compare each number with other numbers in mylist
 
 
-def sum_is_seventeen(l, k):
+def list_sum(l, n):
     for i in range(len(l)):
         print("Processing number at position i: ", i)
         print(l[i])
         for j in range(len(l)):
             if i == j:
                 pass
-            elif l[i] + l[j] == k:
-                print("Found number ", l[i], " + ", l[j], " is ", k, ".")
+            elif l[i] + l[j] == n:
+                print("Found number ", l[i], " + ", l[j], " is ", n, ".")
                 return True
 
 
-if sum_is_seventeen(mylist, k):
+if list_sum(mylist, k):
     print("Any two numbers of ", mylist, " adds up to ", k)
 else:
     print("No two numbers of the list ", mylist, " adds up to ", k)

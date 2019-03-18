034aec600828f4b7d9b6ad74c112b787605ef4b4
Robin Cernin <cerninr@gmail.com>
2019-03-18 16:06:08 +1000

    Create dynamic list on the fly.
    Signed-off-by: Robin Cernin <cerninr@gmail.com>

--- Feb27_2019.py
+++ Feb27_2019.py
@@ -2,13 +2,27 @@
 # -*- coding: cp1252 -*-
 
 import time
+import random
 
 # Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-mylist = [10, 15, 11, 5, 20]
-sortedMylist = [5, 10, 11, 15, 20]
-k = 20
+# mylist = [10, 15, 11, 5, 20]
+# sortedMylist = [5, 10, 11, 15, 20]
+# k = 30
+n = 1000000
+
+
+def generate_input(n):
+    start = time.time()
+    randomlist = [None] * n
+    for i in range(0, len(randomlist)):
+        randomlist[i] = random.randint(1, n)
+    sortedrandomlist = sorted(randomlist)
+    randomnumber = random.randint(1, n)
+    end = time.time()
+    print("Elapsed time: ", end - start)
+    return randomnumber, randomlist, sortedrandomlist
 
 # Brute force method where we compare each number with other numbers in mylist
 # Slow method
@@ -26,7 +40,7 @@ def list_sum_two_iter(l, n):
                 # print("Found number ", l[i], " + ", l[j], " is ", n, ".")
                 end = time.time()
                 print("Elapsed time: ", end - start)
-                return True
+                return True, l[i], l[j]
 
 # Fast method (required sorted list) doesn't require two for loops
 
@@ -40,7 +54,7 @@ def list_sum_linear(l,n):
         if l[low_val_index] + l[high_val_index] == n:
             end = time.time()
             print("Elapsed time: ", end - start)
-            return True
+            return True, l[low_val_index], l[high_val_index]
         elif l[low_val_index] + l[high_val_index] > n:
             high_val_index -= 1
         elif l[low_val_index] + l[high_val_index] < n:
@@ -48,14 +62,29 @@ def list_sum_linear(l,n):
     return False
 
 
-if list_sum_linear(sortedMylist, k):
-    print("list_sum_linear: Any two numbers of ", mylist, " adds up to ", k)
-else:
-    print("list_sum_linear: No two numbers of the list ", mylist, " adds up to ", k)
+print("==================STEP 1==========================")
+print("Generating input list with ", n, "items.")
 
-print("============================================")
+randomnumber, randomlist, sortedrandomlist = generate_input(n)
 
-if list_sum_two_iter(mylist, k):
-    print("list_sum_two_iter: Any two numbers of ", mylist, " adds up to ", k)
+print("==================STEP 2==========================")
+print("Searching for two numbers using linear method that adds up to ", randomnumber, " within ", n, "items.")
+
+boolean, n1, n2 = list_sum_linear(sortedrandomlist, randomnumber)
+
+if boolean is True:
+    # print(sortedrandomlist)
+    print("list_sum_linear: Found ", n1, " and ", n2, " adds up to ", randomnumber)
 else:
-    print("list_sum_two_iter: No two numbers of the list ", mylist, " adds up to ", k)
+    print("list_sum_linear: No two numbers found that adds up to ", randomnumber)
+
+print("==================STEP 3==========================")
+print("Searching for two numbers using iterating method that adds up to ", randomnumber, " within ", n, "items.")
+
+boolean, n1, n2 = list_sum_two_iter(randomlist, randomnumber)
+
+if boolean is True:
+    # print(randomlist)
+    print("list_sum_two_iter: Found ", n1, " and ", n2, " adds up to ", randomnumber)
+else:
+    print("list_sum_two_iter: No two numbers found that adds up to ", randomnumber)

70ab2909e3508097ff2e87f06421ae377c584bf8
Robin Cernin <cerninr@gmail.com>
2019-03-20 08:49:49 +1000

    Adding examples of print statements
    Signed-off-by: Robin Cernin <cerninr@gmail.com>

---
+++ examples/Print.py
@@ -0,0 +1,9 @@
+
+name = "John"
+age = 19
+
+print('Hello %s, your age is %d' % (name, age))
+print('Hello %(n)s, your age is %(a)d' % {'n': name, 'a': age})
+print('Hello {}, your age is {}'.format(name, age))
+print('Hello {n}, your age is {a}'.format(n=name, a=age))
+print('Hello ' + str(name) + ', your age is ' + str(age))

02a7dea19daee2c9083dc902318f99ebf7d3e2df
Robin Cernin <cerninr@gmail.com>
2019-03-20 18:21:49 +1000

    Updating folder structure
    Signed-off-by: Robin Cernin <cerninr@gmail.com>

---
+++ Data Structures and Algorithms/Binary_Search.py
@@ -0,0 +1,9 @@
+def binary_search(input_array, value):
+
+    return -1
+
+test_list = [1,3,9,11,15,19,29]
+test_val1 = 25
+test_val2 = 15
+print binary_search(test_list, test_val1)
+print binary_search(test_list, test_val2)

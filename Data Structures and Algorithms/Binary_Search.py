1a7c7bc54834a00b6a1bb73ca1967e966e3beb6e
Robin Cernin <cerninr@gmail.com>
2019-03-22 12:07:52 +1000

    Updating algorithm for Binary Search
    Signed-off-by: Robin Cernin <cerninr@gmail.com>

--- Data Structures and Algorithms/Binary_Search.py
+++ Data Structures and Algorithms/Binary_Search.py
@@ -2,7 +2,10 @@
 
 
 def binary_search(input_array, value):
-    print("looking for value:", value)
+    print("looking for index of value:", value)
+    print("in list:", input_array)
+    print("====")
+    original_list = input_array
 
     for j in range(0, round(len(input_array) / 2)):
         left, right = [], []
@@ -18,15 +21,17 @@ def binary_search(input_array, value):
 
         if value > left[-1] and len(right) > 1:
             print("value not found in left, trying right.")
+            print("====")
             input_array = right
         elif value < right[0] and len(left) > 1:
             print("value not found in right, trying left.")
+            print("====")
             input_array = left
 
         if value == right[0] and len(right) == 1:
-            return value
+            return original_list.index(value)
         elif value == left[0] and len(left) == 1:
-            return value
+            return original_list.index(value)
     return -1
 
 test_list = [1,2,3,4,5,6,7,8,9]

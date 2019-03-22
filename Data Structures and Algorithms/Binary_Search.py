69be9ce70b7ec5f7099ee8cf96d008ac474b6f85
Robin Cernin <cerninr@gmail.com>
2019-03-22 12:00:54 +1000

    Adding Binary Search algo using two loops
    Signed-off-by: Robin Cernin <cerninr@gmail.com>

--- Data Structures and Algorithms/Binary_Search.py
+++ Data Structures and Algorithms/Binary_Search.py
@@ -1,9 +1,36 @@
-def binary_search(input_array, value):
+# Binary Search using iterative solution of two loops
 
+
+def binary_search(input_array, value):
+    print("looking for value:", value)
+
+    for j in range(0, round(len(input_array) / 2)):
+        left, right = [], []
+
+        for i in range(0, len(input_array)):
+            if i < len(input_array) / 2:
+                left.append(input_array[i])
+            elif i >= len(input_array) / 2:
+                right.append(input_array[i])
+
+        print("left: ", left)
+        print("right: ", right)
+
+        if value > left[-1] and len(right) > 1:
+            print("value not found in left, trying right.")
+            input_array = right
+        elif value < right[0] and len(left) > 1:
+            print("value not found in right, trying left.")
+            input_array = left
+
+        if value == right[0] and len(right) == 1:
+            return value
+        elif value == left[0] and len(left) == 1:
+            return value
     return -1
 
-test_list = [1,3,9,11,15,19,29]
-test_val1 = 25
-test_val2 = 15
-print binary_search(test_list, test_val1)
-print binary_search(test_list, test_val2)
+test_list = [1,2,3,4,5,6,7,8,9]
+test_val1 = 9
+test_val2 = 12
+print(binary_search(test_list, test_val1))
+print(binary_search(test_list, test_val2))

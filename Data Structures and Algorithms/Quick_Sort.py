bad10a13b4e83663535392fa33c1b988f6a0e293
Robin Cernin <cerninr@gmail.com>
2019-03-29 17:51:35 +1000

    Updating Quick Sort Algorithm
    Signed-off-by: Robin Cernin <cerninr@gmail.com>

--- Data Structures and Algorithms/Quick_Sort.py
+++ Data Structures and Algorithms/Quick_Sort.py
@@ -2,19 +2,32 @@
 
 
 def quick_sort(l):
-    pivot = len(l) - 1
+    if len(l) < 2:
+        return
+
     counter = 0
-    while counter != pivot:
-        if l[counter] > l[pivot]:
-            l[pivot], l[pivot-1] = l[pivot-1], l[pivot]
-            l[counter], l[pivot] = l[pivot], l[counter]
-            pivot -= 1
-        elif l[counter] < l[pivot]:
-            counter += 1
+    pivot = len(l) - 1
+    pivot = divide(l, counter, pivot)
 
     return l
 
+
+def divide(l, counter, pivot):
+
+    while counter != pivot:
+        if l[counter] > l[pivot] and pivot - 1 != counter:
+            l[pivot], l[pivot-1] = l[pivot-1], l[pivot]
+            l[counter], l[pivot] = l[pivot], l[counter]
+            pivot -= 1
+        elif l[counter] > l[pivot] and pivot - 1 == counter:
+            l[pivot], l[pivot - 1] = l[pivot - 1], l[pivot]
+        elif l[counter] < l[pivot]:
+            counter += 1
+
+    return pivot
+
 # TODO: This is first part
+
 
 array = [8, 3, 1, 7, 0, 10, 2]
 print(quick_sort(array))

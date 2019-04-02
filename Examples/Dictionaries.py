919dd52ffeab5d6e5304c6c7d852b1b0d70e64d3
Robin Cernin <cerninr@gmail.com>
2019-04-02 13:05:47 +1000

    Adding dictionaries example
    Signed-off-by: Robin Cernin <cerninr@gmail.com>

---
+++ Examples/Dictionaries.py
@@ -0,0 +1,22 @@
+
+redhat = {}
+redhat['r'] = 1
+redhat['e'] = 2
+redhat['d'] = 3
+redhat['h'] = 4
+redhat['a'] = 5
+redhat['t'] = 6
+
+print(redhat)
+
+redhat_list = {}
+redhat_list['r'] = [1]
+redhat_list['e'] = [2]
+redhat_list['d'] = [3]
+redhat_list['h'] = [4]
+redhat_list['a'] = [5]
+redhat_list['t'] = [6]
+
+redhat_list['e'].append(5)
+
+print(redhat_list)

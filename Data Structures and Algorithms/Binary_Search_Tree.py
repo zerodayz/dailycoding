ac0c203d71e5cd13123b51ee13f58e60d06fd634
Robin Cernin <cerninr@gmail.com>
2019-04-11 18:28:24 +1000

    Adding BST Algorithm

---
+++ Data Structures and Algorithms/Binary_Search_Tree.py
@@ -0,0 +1,59 @@
+# Binary Search Tree #1 First attempt
+
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def insert(self, new_val):
+        current = self.root
+        self.reinsert(new_val, current)
+
+    
+    def reinsert(self, new_val, current):
+        if current.left is None and new_val < current.value:
+            current.left = Node(new_val)
+            return True
+        elif current.right is None and new_val > current.value:
+            current.right = Node(new_val)
+            return True
+
+        if new_val > current.value:
+            current = current.right
+            self.reinsert(new_val, current)
+        elif new_val < current.value:
+            current = current.left
+            self.reinsert(new_val, current)
+
+    def search(self, find_val):
+        return False
+    
+# Set up tree
+tree = BinarySearchTree(4)
+
+# Insert elements
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+tree.insert(5)
+
+# tree node looks like:
+#
+#         4
+#        /  \
+#       2    5
+#      / \
+#     1   3
+#
+
+
+# Should be True
+print(tree.search(4))
+# Should be False
+print(tree.search(6))

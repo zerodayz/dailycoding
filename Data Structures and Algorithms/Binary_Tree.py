dc20127d2b190dde402081d5e1c4c9a66487714a
Robin Cernin <cerninr@gmail.com>
2019-04-11 16:49:24 +1000

    Updating Binary Tree First attempt

--- Data Structures and Algorithms/Binary_Tree.py
+++ Data Structures and Algorithms/Binary_Tree.py
@@ -1,4 +1,4 @@
-# Binary Tree #1
+# Binary Tree #1 First attempt
 
 
 class Node(object):
@@ -72,12 +72,10 @@ tree.root.left.right = Node(5)
 #
 
 
-# Test search
 # Should be True
 print(tree.search(4))
 # Should be False
 print(tree.search(6))
 
-# Test print_tree
 # Should be 1-2-4-5-3
 print(tree.print_tree())

103fe4a100cbc67ead19757d24be9007753c4987
Robin Cernin <rcerninr@redhat.com>
2019-06-20 16:33:04 +1000

    Modification to Exceptions

--- Examples/Exceptions.py
+++ Examples/Exceptions.py
@@ -6,3 +6,9 @@ try:
     print('Hello ' + str(name) + ', your age is ' + int(age))
 except Exception as e:
     print('Exception caught: %s' % e)
+
+try:
+    raise NameError('HiThere')
+except NameError:
+    print 'An exception flew by!'
+    raise

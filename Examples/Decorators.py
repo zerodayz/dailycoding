a2d98a0cda6c76ee6689cd3d39fc8d14e5bc079c
Robin Cernin <rcerninr@redhat.com>
2019-06-23 17:57:25 +1000

    Updating decorator

--- Examples/Decorators.py
+++ Examples/Decorators.py
@@ -3,8 +3,11 @@ def my_decorator(func):
         print("Something is happening before the function is called.")
         func()
         print("Something is happening after the function is called.")
-    return wrapper
+    return wrapper  
 
 @my_decorator
 def say_whee():
     print("Whee!")
+
+
+say_whee()
